d28bb7a2c94332c70088a12bd2d7c0bce0d495b9
AmandaaViana <amandaviana.ahv@gmail.com>
2025-11-30 15:58:24 -0300

    Implement merge sort and update menu options

--- Aula 10 -  Ordenação Eficiente de Vetores/ordenacao.c
+++ Aula 10 -  Ordenação Eficiente de Vetores/ordenacao.c
@@ -2,51 +2,76 @@
 #include <stdlib.h>
 #define TAM_MAX 10
 
-int Lista[TAM_MAX];
-void criaLista(int *Lista);
 void inserir(int vet[]);
-void bubbleSort(int *vet, int n);
+void merge(int *vet, int *aux, int esq, int meio, int dir);
+void mergeSort(int *vet, int *aux, int esq, int dir); //divisão do vetor em vetores menores
+void imprimeVetor(int vet[]);
 
-
-
-void inserir(int vet[])
-
-    int i,valor;
+void inserir(int vet[]){
+    int i,j,valor;
+    
     for (i=0; i < TAM_MAX; i++ ){
-        printf("Valor inserido: ");
+        printf("Valor inserido:");
         scanf("%d", &valor);
         vet[i] = valor;
     }
 }
 
-void bubbleSort(int *vet, int n){
-    int i, j, trocou, aux;
-    j=1;
-    trocou=1; 
-    while ((j<n) && trocou){
-        trocou=0; 
-        for(i=0; i<(n-j); i++){
-            if(vet[i]>vet[i+1]){
-                aux= vet[i];
-                vet[i]=vet[i+1];
-                vet[i+1]=aux;
-                trocou=1; 
-            }
-        }
-        j++;
+void merge(int *vet, int *aux, int esq, int meio, int dir){
+    int l1, l2, i;
+    
+    for (l1 = esq, l2 = meio + 1, i = esq; l1 <= meio && l2 <= dir; i++){
+        if (vet[l1] <= vet[l2])
+            aux[i] = vet[l1++];
+        else
+           aux[i] = vet[l2++]; 
     }
+    while(l1 <= meio)
+        aux[i++] = vet[l1++];
+        
+    while(l2 <= dir)
+        aux[i++] = vet[l2++];
+        
+    for (i = esq; i <= dir; i++)
+        vet[i] = aux[i];
+}
+
+void mergeSort(int *vet, int *aux, int esq, int dir){
+    int meio;
+    
+    if(esq < dir){
+        meio = (esq + dir) /2;
+        mergeSort(vet, aux, esq, meio);
+        mergeSort(vet, aux, meio + 1 , dir);
+        merge(vet, aux, esq, meio, dir);
+    } else {
+        return;
+    }
+}
+
+void imprimeVetor(int vet[]){
+    int i;
+    printf("Vetor: [");
+    for (i = 0; i < TAM_MAX; i++) {
+        printf("%d", vet[i]);
+        if (i < TAM_MAX - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
 }
 
 int main(){
     
-    int busca[TAM_MAX];
-    int opcao, valor, pos;
+    int ordenacao[TAM_MAX];
+    int aux[TAM_MAX];
+    int opcao;
     
     do {
         printf("\n----- MENU DE OPÇÕES -----\n");
         printf("1 - Inserir elemento\n");
-        printf("2 - Buscar um elemento (Busca sequencial)\n");
-        printf("3 - Buscar um elemento (Busca binaria)\n");
+        printf("2 - Visualizar vetor atual\n");
+        printf("3 - Odenanar elementos\n");
         printf("0 - Sair\n");
         printf("Escolha a opção: ");
 
@@ -57,30 +82,16 @@ int main(){
 
         switch (opcao) {
             case 1:
-                inserir(busca);
+                inserir(ordenacao);
                 break;
 
             case 2:
-                printf("Digite o valor a ser buscado: ");
-                scanf("%d", &valor);
-                pos = buscaSeq(busca, valor);
-                
-                if (pos != -1) {
-                    printf("Valor encontrado na posiçao [%d]\n", pos);
-                } else {
-                    printf("Valor %d não encontrado.\n", valor);
-                }
+                imprimeVetor(ordenacao);
                 break;
 
             case 3:
-                printf("Digite o valor a ser buscado: ");
-                scanf("%d", &valor);
-                pos = buscaBin(busca,0, TAM_MAX, valor);
-                if (pos != -1) {
-                    printf("Elemento encontrado na posiçao [%d]\n", pos);
-                } else {
-                    printf("Valor %d não encontrado.\n", valor);
-                }
+                mergeSort(ordenacao, aux, 0, TAM_MAX - 1); 
+                imprimeVetor(ordenacao);
                 break;
                 
             case 0:
